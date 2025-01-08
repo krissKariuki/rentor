fa0ce3737c1fc4ab63af1e2dbe915e85ec278387
krisskariuki <chri570ph3rk4riuki@gmail.com>
2025-01-08 09:46:10 +0300

    added functionality for category nav buttons and some styling as well

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,7 +1,9 @@
+import {useState} from "react"
 import Categorytab from "./Categorytab"
 
 export default function Categories()
 {
+    const [activeCategory,setActiveCategory]=useState('new')
 
     const categories=[
         {icon:'new_releases',name:'new'},
@@ -19,18 +21,38 @@ export default function Categories()
         {icon:'location_city',name:'Town House'},
         {icon:'cottage',name:'Cottage'},
     ]
+
+    const handleCategorytabClick=(category)=>
+    {
+        setActiveCategory(category)
+    }
+
+    const handleCategoriesnavrightClick=()=>{
+        const categories=document.querySelector('.categories')
+        categories.scrollTo({left:categories.scrollLeft+800,behavior:'smooth'})
+    }
+    const handleCategoriesnavleftClick=()=>{
+        const categories=document.querySelector('.categories')
+        categories.scrollTo({left:categories.scrollLeft-800,behavior:'smooth'})
+    }
     return(
         <>
-        <button className="material-symbols-outlined categories-nav cn-left">chevron_left</button>
+        <button className="material-symbols-outlined categories-nav cn-left" onClick={handleCategoriesnavleftClick}>chevron_left</button>
         <div className="categories">
             {
                 categories.map((category,index)=>
                 {
-                    return <Categorytab key={index} icon={category.icon} name={category.name}/>
+                    return <Categorytab 
+                    key={index} 
+                    icon={category.icon} 
+                    name={category.name}
+                    categoryState={activeCategory} 
+                    handleCategorytabClick={handleCategorytabClick}
+                    />
                 })
             }
         </div>
-        <button className="material-symbols-outlined categories-nav cn-right">chevron_right</button>
+        <button className="material-symbols-outlined categories-nav cn-right" onClick={handleCategoriesnavrightClick}>chevron_right</button>
 
         </> 
     )
--- frontend/src/components/Categorytab.jsx
+++ frontend/src/components/Categorytab.jsx
@@ -1,9 +1,11 @@
 export default function Categorytab(props)
 {
     return(
-        <p className="category-tab">
+        <button 
+        className={props.categoryState===props.name?'category-tab active-categorytab':'category-tab'}
+        onClick={()=>props.handleCategorytabClick(props.name)}>
             <span className="material-symbols-outlined category-icon">{props.icon}</span>
             <span>{props.name}</span>
-        </p>
+        </button>
     )
 }
